bf31268a9526ba5ac94b3df188a8d63c916e5119
Ricardo Sardón <rst.bcnb@gmail.com>
2021-10-15 13:26:40 +0200

    tortilla/commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,9 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    es2020: true
+    es2020: true,
+    node: true,
+    jest: true
   },
   extends: 'eslint:recommended',
   parserOptions: {
